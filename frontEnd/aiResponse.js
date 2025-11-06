7e9dbd1163690200291a27a6e0c2a9b98810ea90
NinjazGY2497 <gyagau@gmail.com>
2025-11-06 01:13:19 -0500

    Fixed typo: aiResponse --> ai-response

--- frontEnd/aiResponse.js
+++ frontEnd/aiResponse.js
@@ -1,5 +1,5 @@
 export async function aiResponse(prompt) {
-  const flaskServerUrl = "https://hackathonCrewRAAG.pythonanywhere.com/aiResponse"; // Flask Server URL: https://hackathonCrewRAAG.pythonanywhere.com/aiResponse
+  const flaskServerUrl = "https://hackathonCrewRAAG.pythonanywhere.com/ai-response"; // Flask Server URL: https://hackathonCrewRAAG.pythonanywhere.com/ai-response
   const sendingData = {
     prompt: prompt
   };
