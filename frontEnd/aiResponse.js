94cf02ad6c8a6b5403e8afd0f5a5efa5e895dd4d
NinjazGY2497 <gyagau@gmail.com>
2026-04-13 20:57:33 -0400

    changed to local server url

--- frontEnd/aiResponse.js
+++ frontEnd/aiResponse.js
@@ -1,5 +1,5 @@
 export async function aiResponse(prompt) {
-  const flaskServerUrl = "http://127.0.0.1:2497/ai-response"; // Flask Server URL: https://hackathonCrewRAAG.pythonanywhere.com/ai-response
+  const flaskServerUrl = "http://127.0.0.1:2497/terralytics/ai-response"; // Flask Server URL: https://hackathonCrewRAAG.pythonanywhere.com/ai-response
   const sendingData = {
     prompt: prompt
   };
